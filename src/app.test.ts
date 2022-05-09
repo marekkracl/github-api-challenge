ef6fe65fbeb86ec89bff4d0ee32c7ed8d1aba246
Marek Kracl <marek.kracl@gmail.com>
2022-05-08 19:27:10 -0500

    add and unit test formatResult

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,4 +1,4 @@
-import createServer from "./app";
+import { createServer, formatResult } from "./app";
 import request from "supertest";
 
 const app = createServer();
@@ -8,6 +8,28 @@ describe("Server Started", () => {
     it("should return 200 with a valid owner/repo", (done) => {
       //const application = app;
       request(app).get("/marekkracl/github-api-challenge").expect(200, done);
+    });
+  });
+});
+
+describe("Unit Tests", () => {
+  describe("formatResult", () => {
+    it("should return a formatted object", () => {
+      const result = {
+        id: 1,
+        number: 123,
+        title: "test",
+        user: {
+          login: "testUser",
+        },
+      };
+      const expected = {
+        id: 1,
+        number: 123,
+        title: "test",
+        author: "testUser",
+      };
+      expect(formatResult(result)).toEqual(expected);
     });
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,14 @@
-import express from "express";
-const app = express();
 import axios, { AxiosResponse } from "axios";
 import express, { Application, Request, Response } from "express";
 
-app.get("/", (req, res) => {
-  res.json("Hello World!");
-});
 const app: Application = express();
 const port: number = parseInt(process.env.PORT, 10) || 3000;
 const token: string = process.env.TOKEN;
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
 //sets token if one was passed, otherwise results will be rate-limited
 const headers = { authorization: token ? ` token ${token}` : "" };
 
-export default function createServer() {
+export function createServer() {
   app.get("/:owner/:repo", async (req: Request, res: Response) => {
     // Get the owner and repo from the URL
     const { owner, repo } = req.params;
@@ -30,4 +22,19 @@ export default function createServer() {
   });
   return app;
 }
+
+/**
+ * Formats a pull request into expected result object
+ * @param pull
+ * @returns Promise<APIResult>
+ */
+export function formatResult(pull: IGitHubResult): APIResult {
+  return {
+    id: pull.id,
+    number: pull.number,
+    title: pull.title,
+    author: pull.user.login,
+  };
+}
+
 createServer();
