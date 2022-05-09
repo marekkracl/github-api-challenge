03bfbe42ee6c863ec5ebbcd0fa850e5e509bbb63
Marek Kracl <marek.kracl@gmail.com>
2022-05-08 19:05:00 -0500

    Update types to include expected Github response

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,11 @@ type APIResult = {
   author: string;
   commit_count?: number;
 };
+
+type IGitHubResult = {
+  //these are the only fields we care about
+  id: number;
+  number: number;
+  title: string;
+  user: { login: string };
+};
